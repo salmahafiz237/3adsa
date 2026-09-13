34a82c0057f9d81a0386941ea8562ff279cf9e0d
Salma Mohamed <salmahafiz237@gmail.com>
2026-09-13 18:22:38 +0300

    navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+import { Navbar } from './components/navbar/navbar';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('adasaProject');
+  
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
