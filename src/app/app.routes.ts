71630e266f38855a027f60043ef00803734accb9
Salma Mohamed <salmahafiz237@gmail.com>
2026-09-15 05:30:15 +0300

    blog & about

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { PrivacyPolicy } from './components/privacy-policy/privacy-policy';
 import { TermsOfService } from './components/terms-of-service/terms-of-service';
 import { Home } from './components/home/home';
+import { Blog } from './components/blog/blog';
+import { About } from './components/about/about';
 
 export const routes: Routes = [
     {path:'', redirectTo:'home', pathMatch:'full'},
     {path:"home", component:Home},
+    {path:"blog", component:Blog},
+    {path:"about", component:About},
     {path:'privacy-policy', component:PrivacyPolicy},
     {path: 'terms-of-service', component:TermsOfService},
 
